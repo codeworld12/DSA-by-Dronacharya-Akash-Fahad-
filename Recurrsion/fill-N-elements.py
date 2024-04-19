87b576d5e0146a0c89e7c35432d7cf6a01bcbd39
Shivam Gupta <87769242+codeworld12@users.noreply.github.com>
2024-04-19 23:41:10 +0530

    Update fill-N-elements.py

--- Recurrsion/fill-N-elements.py
+++ Recurrsion/fill-N-elements.py
@@ -12,6 +12,26 @@ n = int(input())
 ans = fun(n)
 print(sorted(ans))
 
+# 2nd Approach
+
+n = int(input())
+li = [0] * (n+1)        ## declaring array
+def fun(n):
+    if n == 0:
+        li[n] = n
+        return li
+    li[n] = n
+    return fun(n-1)
+
+ans = fun(n)
+print(ans)
+
+
+
+
+
+
+
 
 
 
