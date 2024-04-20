98be9d3bb6a2035316fbbb8c4942e38b8fc40892
codeworld12 <shivamrgupta2001@gmail.com>
2024-04-20 19:09:10 +0530

    Third day prime number

--- Recurrsion/primeNo.py
+++ Recurrsion/primeNo.py
@@ -1,12 +1,31 @@
-coun = 0
+# 1) Appproach
+
+# coun = 0
+# def fun(n,a):
+#     global coun
+#     if a == 1 and coun <1:
+#         return True
+#     if coun > 0:
+#         return False
+#     if n % a == 0:
+#         coun += 1
+#     return fun(n, a-1)
+# n  = int(input("enter : "))
+# ans = fun(n, n-1)
+
+# if ans:
+#     print("It is prime number")
+# else:
+#     print("It is not prime number")
+
+# 2) Approach
+
+# Prime Number
 def fun(n,a):
-    global coun
-    if a == 1 and coun <1:
+    if a == 1:
         return True
-    if coun > 0:
-        return False
     if n % a == 0:
-        coun += 1
+        return False
     return fun(n, a-1)
 n  = int(input("enter : "))
 ans = fun(n, n-1)
